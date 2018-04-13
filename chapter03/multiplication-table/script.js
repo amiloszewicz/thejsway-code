7bd0f777223f28b9007b090cf3f30c250bb1840c
amiloszewicz <miloszewicz.adam@gmail.com>
2018-04-13 20:36:51 +0200

    refactor multiplication-table  task

--- chapter03/multiplication-table/script.js
+++ chapter03/multiplication-table/script.js
@@ -1,6 +1,11 @@
 // https://github.com/bpesquet/thejsway/blob/master/manuscript/chapter04.md#multiplication-table
 
-const number = Number(prompt('Enter number to see multiplication table for this number:'));
+const gameInstruction = 'Enter number to see multiplication table for this number:';
+let number = Number(prompt(gameInstruction));
+
+while (number > 9 || number < 2) {
+    number = Number(prompt(gameInstruction));
+}
 
 for (let i = 1; i <= number; i++) {
     console.log(`${number} * ${i} = ${number * i}`);
