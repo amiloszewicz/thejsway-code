b18e2933e0b14d92b94352a99ad34a1fdafa7ff4
amiloszewicz <miloszewicz.adam@gmail.com>
2018-04-13 20:20:49 +0200

    feat parity task

---
+++ chapter03/parity/script.js
@@ -0,0 +1,9 @@
+// https://github.com/bpesquet/thejsway/blob/master/manuscript/chapter04.md#parity
+
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+      console.log(`${i} is even`);
+    } else {
+      console.log(`${i} is odd`);
+    }
+  }
