5492c1cfb14b8bd81f57d5917c8e42c156322e53
Adam Miloszewicz <adam.miloszewicz@meelogic.com>
2018-06-20 11:13:28 +0200

    feat menuOptions

--- Part_I_Learn-to-code-programs/chapter11_Project-a-social-news-program/script.js
+++ Part_I_Learn-to-code-programs/chapter11_Project-a-social-news-program/script.js
@@ -3,3 +3,24 @@
 const showMenu = () => {
     return prompt(`Choose an option:\n1 : Show links\n2 : Add a link\n3 : Remove a link\n0 : Quit`);
 };
+
+const menuOptions = (option) => {
+    switch(option) {
+        case '0':
+            return;
+        case '1':
+            showLinks();
+            menuOptions(showMenu());
+            break;
+        case '2':
+            storeLinks(addLink());
+            menuOptions(showMenu());
+            break;
+        case '3':
+            removeLink();
+            menuOptions(showMenu());
+            break;
+        default:
+            menuOptions(showMenu());
+    }
+};
