9dec9354ca68bf3bb63db19e1596e3305a9f106b
Adam Miloszewicz <adam.miloszewicz@meelogic.com>
2018-06-20 11:12:31 +0200

    feat showMenu

--- Part_I_Learn-to-code-programs/chapter11_Project-a-social-news-program/script.js
+++ Part_I_Learn-to-code-programs/chapter11_Project-a-social-news-program/script.js
@@ -1,1 +1,5 @@
 // https://github.com/bpesquet/thejsway/blob/master/manuscript/chapter11.md#objective
+
+const showMenu = () => {
+    return prompt(`Choose an option:\n1 : Show links\n2 : Add a link\n3 : Remove a link\n0 : Quit`);
+};
